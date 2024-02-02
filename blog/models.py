e895682264fa7848ac61520de90c1ee15d39bbf7
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-03 05:35:04 +0700

    Create model Post.

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Post(models.Model):
+    """Модель поста."""
+
+    class Status(models.TextChoices):
+        DRAFT = 'DF', 'Draft'
+        PUBLISHED = 'PB', 'Published'
+
+    title = models.CharField(max_length=250,
+                             verbose_name='Заголовок')
+    slug = models.SlugField(max_length=250)
+    author = models.ForeignKey(User,
+                               on_delete=models.CASCADE,
+                               verbose_name='Автор поста',
+                               related_name='blog_posts')
+    body = models.TextField()
+    publish = models.DateTimeField(default=timezone.now,
+                                   verbose_name='Дата публикации')
+    created = models.DateTimeField(auto_now_add=True,
+                                   verbose_name='Дата создания')
+    updated = models.DateTimeField(auto_now=True,
+                                   verbose_name='Дата обновления')
+    status = models.CharField(max_length=2,
+                              choices=Status.choices,
+                              default=Status.DRAFT)
+
+    class Meta:
+        ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
+
+    def __str__(self):
+        return self.title

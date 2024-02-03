cff96b6368b60a66f190819f18bb464d726e0e1f
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-03 13:37:09 +0700

    Create PublishedManager.

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,13 @@ from django.utils import timezone
 from django.contrib.auth.models import User
 
 
+class PublishedManager(models.Manager):
+    """Manager for published posts."""
+
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
+
+
 class Post(models.Model):
     """Модель поста."""
 
@@ -27,6 +34,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                               choices=Status.choices,
                               default=Status.DRAFT)
+
+    objects = models.Manager()
+    published = PublishedManager()
 
     class Meta:
         ordering = ['-publish']

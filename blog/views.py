73711ec7098c18b9fa0653a96ad7b9f636190233
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-04 07:45:26 +0700

    Create view 'post_detail'.

--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
 from .models import Post
 
 
 def post_list(request):
     """
     Displays a list of posts on the main page
-    :param request:
     :return: all posts
     """
-    posts = Post.publish.all()
+    posts = Post.published.all()
     return render(request, 'blog/post/list.html', {'posts': posts})
+
+
+def post_detail(request, id):
+    """
+    Displays the selected post
+    :param request:
+    :param id: post id
+    :return: selected post
+    """
+    post = get_object_or_404(Post, pk=id, status=Post.Status.PUBLISHED)
+    return render(request, 'blog/post/detail.html', {'post': post})

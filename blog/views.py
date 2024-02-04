bbde7000ed19094b429013f9adf8d528d99e567c
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-04 07:17:27 +0700

    Create view 'post_list'.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
+
+def post_list(request):
+    """
+    Displays a list of posts on the main page
+    :param request:
+    :return: all posts
+    """
+    posts = Post.publish.all()
+    return render(request, 'blog/post/list.html', {'posts': posts})

b9df63451a7aba18e43c37cb52f5bafa34c49489
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-07 05:18:44 +0700

    Add 'get_absolute_url' for model Post.

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.urls import reverse
 from django.contrib.auth.models import User
 
 
@@ -46,3 +47,6 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])

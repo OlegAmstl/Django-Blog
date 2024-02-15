7ec0ecca89fd8490bb3ea3eb1a5ea689fec14a8e
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-16 06:49:51 +0700

    Created Comment model.

--- blog/models.py
+++ blog/models.py
@@ -12,7 +12,7 @@ class PublishedManager(models.Manager):
 
 
 class Post(models.Model):
-    """Модель поста."""
+    """Post model."""
 
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -55,3 +55,24 @@ class Post(models.Model):
                              self.publish.month,
                              self.publish.day,
                              self.slug])
+
+
+class Comment(models.Model):
+    """Comment model."""
+
+    post = models.ForeignKey(Post,
+                             on_delete=models.CASCADE,
+                             related_name='posts')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ['created']
+        indexes = [models.Index(fields=['created'])]
+
+    def __str__(self):
+        return f'Comment by {self.name} on {self.post}'

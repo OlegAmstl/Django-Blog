0736477e76b1ef8bf58589d20d6aa7a962d76fd6
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-18 06:19:49 +0700

    Install django-taggit.

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 from django.urls import reverse
 from django.contrib.auth.models import User
+from taggit.managers import TaggableManager
 
 
 class PublishedManager(models.Manager):
@@ -38,6 +39,7 @@ class Post(models.Model):
 
     objects = models.Manager()
     published = PublishedManager()
+    tags = TaggableManager()
 
     class Meta:
         ordering = ['-publish']

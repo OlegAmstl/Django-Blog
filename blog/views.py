33b0a7cd616c1ee7c0b5bfedc3b43a77e1a010d7
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-21 05:29:34 +0700

    List of similar posts - template.

--- blog/views.py
+++ blog/views.py
@@ -9,6 +9,7 @@ from taggit.models import Tag
 from .models import Post, Comment
 from .forms import EmailPostForm, CommentForm
 
+
 # TODO: Replace function with class
 # class PostListView(ListView):
 #     """Displays a list of  posts on the main page"""
@@ -42,7 +43,6 @@ def post_list(request, tag_slug=None):
                    'tag': tag})
 
 
-
 # TODO: Replacing a function with class
 def post_detail(request, year, month, day, post):
     """
@@ -66,16 +66,17 @@ def post_detail(request, year, month, day, post):
 
     # List of similar posts
     post_tags_ids = post.tags.values_list('id', flat=True)
-    simular_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(
         id=post.id
     )
-    simular_posts = simular_posts.annotate(same_tags=Count('tags')).order_by(
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by(
         '-same_tags', '-publish'
     )[:4]
-    return render(request, 'blog/post/detail.html', {'post': post,
-                                                     'comments': comments,
-                                                     'form': form,
-                                                     'simular_posts': simular_posts})
+    return render(request,
+                  'blog/post/detail.html', {'post': post,
+                                            'comments': comments,
+                                            'form': form,
+                                            'similar_posts': similar_posts})
 
 
 def post_share(request, post_id):

352c7ea6429634d7973e375773135d9511fd3fd2
OlegAmstl <99185070+OlegAmstl@users.noreply.github.com>
2024-02-24 07:19:17 +0700

    Sitemap begin.

--- mysite/settings.py
+++ mysite/settings.py
@@ -9,6 +9,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SITE_ID = 1
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -19,6 +20,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'blog.apps.BlogConfig',
     'taggit',
+    'django.contrib.sites',
+    'django.contrib.sitemaps'
 ]
 
 MIDDLEWARE = [
